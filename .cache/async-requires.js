23e9e25d29fb1f164e762712e59fa9668863e3e7
rxnaij <richardblue37@gmail.com>
2018-07-25 22:27:54 -0400

    mockup pages in browser

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,7 +5,9 @@ exports.components = {
   "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/dev-404-page.js"),
   "component---src-pages-about-js": require("gatsby-module-loader?name=component---src-pages-about-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/src/pages/about.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/src/pages/index.js"),
-  "component---src-pages-portfolio-js": require("gatsby-module-loader?name=component---src-pages-portfolio-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/src/pages/portfolio.js")
+  "component---src-pages-portfolio-js": require("gatsby-module-loader?name=component---src-pages-portfolio-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/src/pages/portfolio.js"),
+  "component---src-pages-contact-js": require("gatsby-module-loader?name=component---src-pages-contact-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/src/pages/contact.js"),
+  "component---src-pages-portfolio-nycaasc-website-js": require("gatsby-module-loader?name=component---src-pages-portfolio-nycaasc-website-js!/Users/richardlu/Documents/GitHub projects/richardblu.com/src/pages/portfolio/nycaasc-website.js")
 }
 
 exports.json = {
@@ -13,7 +15,9 @@ exports.json = {
   "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/dev-404-page.json"),
   "about.json": require("gatsby-module-loader?name=path---about!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/about.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/index.json"),
-  "portfolio.json": require("gatsby-module-loader?name=path---portfolio!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/portfolio.json")
+  "portfolio.json": require("gatsby-module-loader?name=path---portfolio!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/portfolio.json"),
+  "contact.json": require("gatsby-module-loader?name=path---contact!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/contact.json"),
+  "portfolio-nycaasc-website.json": require("gatsby-module-loader?name=path---portfolio-nycaasc-website!/Users/richardlu/Documents/GitHub projects/richardblu.com/.cache/json/portfolio-nycaasc-website.json")
 }
 
 exports.layouts = {
