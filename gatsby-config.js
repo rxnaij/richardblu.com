385c2b7ceea6d9411f570e8d8b07bed57b8138ff
rxnaij <richardblue37@gmail.com>
2018-08-25 23:51:24 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,19 @@ module.exports = {
     },
     `gatsby-transformer-json`,
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-104214240-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
